54bac95df3718677bc7f9a6ea430dd0c5891ecf7
edufarias4 <eduardogomesbgf@gmail.com>
2024-12-14 18:26:30 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.carousel-container');
+const items = document.querySelectorAll('.carousel-item');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+let currentIndex = 0;
+
+function updateCarousel() {
+  const width = items[0].offsetWidth;
+  container.style.transform = `translateX(${-currentIndex * width}px)`;
+}
+
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex > 0) ? currentIndex - 1 : items.length - 1;
+  updateCarousel();
+});
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex < items.length - 1) ? currentIndex + 1 : 0;
+  updateCarousel();
+});
+
+window.addEventListener('resize', updateCarousel);
